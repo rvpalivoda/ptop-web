94a405d7d32897ab323791169366e4f8b98b472d
Roman Palivoda <rvpalivoda@gmail.com>
2025-08-20 15:29:32 +0200

    Не блокируем клики хедера тостом

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,11 +10,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       position="top-center"
-      className="toaster group"
+      className="toaster pointer-events-none group"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "pointer-events-auto group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
